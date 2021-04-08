ab6a689f1162ce8207f358da3bf29ee1fb9e728f
gerysantoso03 <Gerysantos03@gmail.com>
2021-04-08 23:03:07 +0700

    Responsive notfound page

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -20,15 +20,18 @@
       </div>
     </div>
   </section>
+  <Footer />
 </template>
 
 <script>
 import Navbar from "../components/global/Navbar";
+import Footer from "../components/global/Footer";
 
 export default {
   name: "not-found-page",
   components: {
     Navbar,
+    Footer,
   },
   data() {
     return {
@@ -57,7 +60,7 @@ export default {
 
     .project-not-found {
       cursor: pointer;
-      width: 30%;
+      width: 25%;
       padding: 1rem 0;
       border-radius: 3rem;
       text-align: center;
@@ -101,6 +104,80 @@ export default {
         color: $light-gray;
         font-weight: 500;
         font-size: 1.6rem;
+      }
+    }
+  }
+}
+
+/* Media query - Tablet Styles */
+@media (min-width: $tablet-width) and (max-width: $dekstop-width) {
+  .notfound-wrapper {
+    .notfound-image {
+      width: 50%;
+    }
+
+    .notfound-info {
+      width: 50%;
+
+      .project-not-found {
+        width: 35%;
+        p {
+          font-size: 1.4rem;
+        }
+      }
+      .notfound-title {
+        font-size: 3rem;
+      }
+
+      .notfound-desc,
+      .notfound-desc2 {
+        font-size: 1.6rem;
+      }
+
+      .btn-to-home {
+        width: 40%;
+        a {
+          font-size: 1.4rem;
+        }
+      }
+    }
+  }
+}
+
+/* Media query - Mobile styles */
+@media (max-width: $tablet-width) {
+  .notfound-wrapper {
+    flex-direction: column;
+    align-items: center;
+    .notfound-image {
+      width: 70%;
+    }
+
+    .notfound-info {
+      width: 80%;
+      // border: 1px solid $dark;
+
+      .project-not-found {
+        display: none;
+      }
+      .notfound-title {
+        font-size: 4.5rem;
+      }
+
+      .notfound-desc {
+        font-size: 1.4rem;
+      }
+
+      .notfound-desc2 {
+        display: none;
+      }
+
+      .btn-to-home {
+        padding: 1rem 0;
+        width: 40%;
+        a {
+          font-size: 1.2rem;
+        }
       }
     }
   }
